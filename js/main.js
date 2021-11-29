1d7d533b18da026e814fa53204ad341c5fb02712
Nestormaj <89750713+Nestormaj@users.noreply.github.com>
2021-11-29 17:47:15 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,6 @@ ScrollReveal().reveal('.noticias' , { delay: 500});
 ScrollReveal().reveal('.section' , { delay: 500});
 ScrollReveal().reveal('.section.two' , { delay: 500});
 
+function copy(){
+    alert("NAME: Nestor Martinez. EMAIL: nestorjose0503@gmail.com")
+}
